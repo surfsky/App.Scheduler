611a78fd80c9a6a8c84d754de39c951f9389f9b7
surfsky <surfsky@sina.com>
2018-11-09 11:44:43 +0800

    fix

---
+++ App.Scheduler/Components/Extensions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace App.Components
+{
+    /// <summary>
+    /// 一些常用的扩展
+    /// </summary>
+    internal static class Extensions
+    {
+        /// <summary>字符串是否为空</summary>
+        public static bool IsNullOrEmpty(this string txt)
+        {
+            return String.IsNullOrEmpty(txt);
+        }
+
+        /// <summary>对象是否为空或为空字符串</summary>
+        public static bool IsNullOrEmpty(this object o)
+        {
+            return (o == null) ? true : o.ToString().IsNullOrEmpty();
+        }
+
+        /// <summary>将可空对象转化为字符串</summary>
+        public static string ToText(this object o, string format="{0}")
+        {
+            //return o == null ? "" : o.ToString();
+            return string.Format(format, o);
+        }
+
+        /// <summary>将可空bool对象转化为字符串</summary>
+        public static string ToText(this bool? o, string trueText = "true", string falseText = "false")
+        {
+            return o == null
+                ? ""
+                : (o.Value ? trueText : falseText)
+                ;
+        }
+
+        /// <summary>将可空对象转化为整型字符串</summary>
+        public static string ToIntText(this object o)
+        {
+            if (o == null) return "";
+            else return Convert.ToInt32(o).ToString();
+        }
+
+        /// <summary>将可空对象转化为整型</summary>
+        public static int ToInt32(this object o)
+        {
+            if (o == null) return -1;
+            else return Convert.ToInt32(o);
+        }
+
+        /// <summary>将可空对象转化为时间类型</summary>
+        public static DateTime ToDateTime(this object o)
+        {
+            return o == null ? new DateTime() : DateTime.Parse(o.ToString());
+        }
+
+        /// <summary>转化为逗号分隔的字符串</summary>
+        public static string ToCommaString(this IEnumerable source)
+        {
+            string txt = "";
+            foreach (var item in source)
+                txt += item.ToString() + ",";
+            return txt.TrimEnd(',');
+        }
+
+        /// <summary>转化为整型列表</summary>
+        public static List<int> CastInt(this IEnumerable source)
+        {
+            var result = new List<int>();
+            foreach (var item in source)
+            {
+                result.Add(int.Parse(item.ToString()));
+            }
+            return result;
+        }
+
+        /// <summary>转化为整型列表</summary>
+        public static List<string> CastString(this IEnumerable source)
+        {
+            var result = new List<string>();
+            foreach (var item in source)
+            {
+                result.Add(item.ToString());
+            }
+            return result;
+        }
+
+        /// <summary>转化为枚举列表</summary>
+        public static List<T> CastEnum<T>(this IEnumerable source) where T : struct
+        {
+            var result = new List<T>();
+            foreach (var item in source)
+            {
+                object o = Enum.ToObject(typeof(T), Convert.ToInt32(item));
+                result.Add((T)o);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ App.Scheduler/ScheduleConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using App.Components;
+
+namespace App.Scheduler
+{
+    /// <summary>
+    /// 调度的配置信息
+    /// </summary>
+    public class ScheduleConfig
+    {
+        /// <summary>每次循环休眠毫秒数</summary>
+        public int Sleep { get; set; }
+        public DateTime LogDt { get; set; }
+        public List<Job> Jobs { get; set; }
+
+        // 单例
+        static string _configFile;
+
+        public ScheduleConfig() { }
+        public ScheduleConfig(string configFile)
+        {
+            _configFile = configFile;
+            var cfg = Load(configFile);
+            this.Sleep = cfg.Sleep;
+            this.LogDt = cfg.LogDt;
+            this.Jobs = cfg.Jobs;
+        }
+
+        // 加载
+        static ScheduleConfig Load(string filePath)
+        {
+            if (!File.Exists(filePath))
+                CreateDemo().Save();
+            return SerializeHelper.LoadJson(filePath, typeof(ScheduleConfig)) as ScheduleConfig;
+        }
+
+        // 保存
+        public void Save()
+        {
+            if (!string.IsNullOrEmpty(_configFile))
+               SerializeHelper.SaveJson(_configFile, this);
+        }
+
+        // 创建示例数据
+        static ScheduleConfig CreateDemo()
+        {
+            ScheduleConfig cfg = new ScheduleConfig();
+            cfg.Sleep = 400;
+            cfg.LogDt = DateTime.Now;
+            cfg.Jobs = new List<Job>
+            {
+                new Job()
+            };
+            return cfg;
+        }
+    }
+}
